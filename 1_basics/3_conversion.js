a2ff83265a832aa8f3f9b2cf598c2c1b6c9b6c7e
kokil1135be25-lgtm <kokil1135.be25@chitkarauniversity.edu.in>
2026-02-05 12:28:28 +0000

    yes

---
+++ 1_basics/3_conversion.js
@@ -0,0 +1,11 @@
+let num = "3a"
+// console.log(typeof num);
+
+let valueInNumber = Number(num)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let name = "3"
+let sname = Number(name)
+console.log(typeof sname);
