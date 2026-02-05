3daac1a3ed70f3e5b82beef7b47b6e32744a546a
kokil1135be25-lgtm <kokil1135.be25@chitkarauniversity.edu.in>
2026-02-05 14:13:33 +0000

    ye

--- 1_basics/3_conversion.js
+++ 1_basics/3_conversion.js
@@ -6,6 +6,18 @@ let valueInNumber = Number(num)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
-let name = "3"
-let sname = Number(name)
-console.log(typeof sname);
+// let name = "3"
+// let sname = Number(name)
+// console.log(typeof sname);
+
+
+let per1 ={
+    name: "kokil",
+    rollno : 1135
+}
+let per2 = per1
+per2.name = "kavya"
+
+console.log(per1.name);
+console.log(per2.name);
+
---
+++ 1_basics/4_strings.js
@@ -0,0 +1,27 @@
+const name = "kokilsharma"
+const age = 20
+
+console.log(`My name is ${name} and my age is ${age}.`);
+console.log(name[3]);
+console.log(String(age)[1]);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name);
+console.log(name.charAt(2));
+console.log(name.indexOf('k'));
+
+const newname = name.substring(0,5)
+console.log(newname);
+
+
+const newstring = "  kavoo  "
+console.log(newstring.trim());
+
+const a = "aabbcc"
+console.log(a.replace('a','5'));
+console.log(a.includes('bb'));
+
+const b = "kokil-is-a-good-guy."
+console.log(b.split('-'));
+console.log(b.split('-')[3]);
+
