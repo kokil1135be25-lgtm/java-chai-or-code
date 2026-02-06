428f33d33e0db388b88eaa80b67f860423cf9a0a
kokil1135be25-lgtm <kokil1135.be25@chitkarauniversity.edu.in>
2026-02-06 11:48:51 +0000

    yes

---
+++ 1_basics/5_nums.js
@@ -0,0 +1,20 @@
+const score = 100
+// console.log(score.toString());
+// console.log(typeof score);
+// console.log(score.toFixed(2));
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1,2,3));
+
+// console.log(Math.random());   GIVES VALUES B/W 0 AND 1
+// console.log((Math.random()*10)+1);
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
