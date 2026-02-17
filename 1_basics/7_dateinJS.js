2fd72045462e728d2f9e195524af7fbdf187632d
kokil1135be25-lgtm <kokil1135.be25@chitkarauniversity.edu.in>
2026-02-17 18:08:09 +0000

    yes

---
+++ 1_basics/7_dateinJS.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString);
+
+// let myDate2 = new Date(2023,1,25)
+// console.log(myDate2.toDateString());
+
+// let myDate3 = Date.now()
+// console.log(myDate3);
+
+let myDate4 = new Date()
+console.log(myDate4.getDate());
