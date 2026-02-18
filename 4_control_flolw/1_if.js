0b82ee02ed2a6c4d56895bfb171e28b4a5a01f8f
kokil1135be25-lgtm <kokil1135.be25@chitkarauniversity.edu.in>
2026-02-18 20:24:09 +0000

    yes

---
+++ 4_control_flolw/1_if.js
@@ -0,0 +1,14 @@
+const arr1 = []
+if(arr1.length === 0){
+    console.log("Empty");
+}
+
+const obj1 = {}
+if((Object.keys(obj1)).length === 0){
+    console.log("Empty");
+    
+}
+
+const a =100
+a < 50 ? console.log("less than 50"): console.log("greater than 50");
+
---
+++ 5_loops/1_one.js
@@ -0,0 +1,7 @@
+let arr1 = [1,2,3,4]
+
+for(i=0;i<arr1.length;i++){
+    const element = arr1[i]
+    console.log(element);
+    
+}
